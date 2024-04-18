7feee8ab5ea6a243ace646a6b3ed6687364d5594
Imad Dabligi <imad.dabligi@gmail.com>
2024-04-18 18:29:23 +0100

    Learned Lists rangefunc

--- python_practice/app.py
+++ python_practice/app.py
@@ -17,9 +17,32 @@ print('Hello world')
 # print("Drink water")
 
 
-#--------------Loops--------------
+#--------------While_Loops--------------
 
-i = 1
-while i <= 1_0:
-    print(i * '+')
-    i+=1
+# i = 1
+# while i <= 1_0:
+#     print(i * '+')
+#     i+=1
+
+#--------------Lists--------------
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.insert(0, -1)
+# numbers.append(6)
+# print(5 in numbers)
+# print(numbers) 
+# print("lenght of list is : " + str(len(numbers)))
+# print(10 in numbers)
+
+#--------------For_Loops--------------
+
+# numbers = [1, 2, 3, 4, 5]
+# for it in numbers:
+#     print(it)
+
+#--------------Func-range()--------------
+
+numbers = range(5, 10, 2)
+
+for num in numbers:
+    print(num)
