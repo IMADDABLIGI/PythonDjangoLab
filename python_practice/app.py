0a62fda79bf0d592cfa8576c9c94c749ca24f16f
Imad Dabligi <imad.dabligi@gmail.com>
2024-04-18 16:01:56 +0100

    Learning Python

---
+++ python_practice/app.py
@@ -0,0 +1,25 @@
+print('Hello world')
+
+# name = input("Enter your name : ")
+# birth_year= input("Enter your year of birth : ")
+# print(name)
+# print(2024 - int(birth_year))
+
+
+#--------------Conditions-------------- 
+# tempature = int(input("Enter today's tempature : "))
+# if (tempature > 30):
+#     print("It's a hot day")
+# elif (tempature < 15):
+#     print("It's a cold day")
+# else:
+#     print("It's a good day")
+# print("Drink water")
+
+
+#--------------Loops--------------
+
+i = 1
+while i <= 1_0:
+    print(i * '+')
+    i+=1
