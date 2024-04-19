2c5f1c579b7e4b32ead4558ef40bd3396b2b3e3f
Imad Dabligi <imad.dabligi@gmail.com>
2024-04-19 18:27:41 +0100

    Learned tupels Exception OpenFile

--- python_practice/app.py
+++ python_practice/app.py
@@ -9,7 +9,7 @@ print('Hello world')
 #--------------Conditions-------------- 
 # tempature = int(input("Enter today's tempature : "))
 # if (tempature > 30):
-#     print("It's a hot day")
+#     print("It's a hot day")             #Indent
 # elif (tempature < 15):
 #     print("It's a cold day")
 # else:
@@ -54,64 +54,4 @@ print('Hello world')
  
 # welcome("IMAD", 20)
 # print(round(1.6))
-
-
-#--------------Open-File--------------
-#--------------Write-Append--------------
-    
-# with open("text1.txt", "w") as file:
-#     file.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
-
-# with open("text1.txt", "a") as file:
-#     file.write("\n Birds flyins high.")
-
-#--------------Try-Except-Block--------------
-#--------------Read-Only--------------
-
-# try:
-#     with open("text1.txt") as file: #by using with block it makes sure that the file is safely oppened and safely closed
-#         print("||---" , file.read() + "---||")
-# except FileNotFoundError:
-#     print("File not exsited !")
-
-# try:
-#     print(file.closed) #Checking if file got closed
-# except Exception as e:
-#     print(e)
-
-#--------------Function-Optional-Argument--------------
-    
-# def increment(num, by=1): #The optional argument always comes after the required argument not before !
-#     return num + by
-
-# print(increment(2 + 5))
-
-#--------------Tuples-*arg------------- #You can't append a new value to the tuples
-#--------------Methodes1
-# def multiplication(*numbers):
-#     i = 0
-#     count = 1
-#     max = len(numbers)
-    
-#     while i < max:
-#         count *= numbers[i]
-#         i += 1
-#     return count
-
-#--------------Methodes2
-# def multiplication(*numbers):
-#     count = 1
-    
-#     for number in numbers:
-#         count *= number
-#     return count
-
-# print(multiplication(4, 5, 10))
-
-#--------------Dictinories-**arg--------------
-
-def printing(**user):
-    print(user["name"])
-    print(user["city"])
-
-printing(name="IMAD", age=23, school=1337, city="CASABLANCA")
+fizz_buzz(99)
---
+++ python_practice/fizz_buzz.py
@@ -0,0 +1,11 @@
+def fizz_buzz(num: int) -> None:
+    while num <= 100:
+        if num % 3 == 0 and num % 5 == 0:
+            print("Fizz_Buzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+        num += 1
